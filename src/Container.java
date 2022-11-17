1cafb21d1f1019fb0c61e5af591718ad36697525
Dima <dmitriy.matveev89@inbox.ru>
2022-11-18 00:16:59 +0300

    Initial commit

---
+++ src/Container.java
@@ -0,0 +1,7 @@
+public class Container
+{
+    public int count_01;
+    public int count_02;
+    public int count_03;
+//    public Integer count;
+}
---
+++ src/Main.java
@@ -0,0 +1,35 @@
+public class Main {
+    public static void main(String[] args) {
+        Container container = new Container();
+        container.count_01 = 12345;
+        container.count_02 = 10;
+        container.count_03 = 5059191;
+
+        int sumNumber = Main.sumDigits(container.count_01);
+        System.out.println("Сумма чисел в переменной  " + container.count_01 + " = " + sumNumber);
+        sumNumber = Main.sumDigits(container.count_02);
+        System.out.println("Сумма чисел в переменной  " + container.count_02 + " = " + sumNumber);
+        sumNumber = Main.sumDigits(container.count_03);
+        System.out.println("Сумма чисел в переменной  " + container.count_03 + " = " + sumNumber);
+    }
+
+    public static Integer sumDigits(int ivNumber)
+//    public Integer sumDigits(Integer number)
+    {
+        int rvSum = 0;
+//            Conv Строка->число
+        String lvNumberStrVar1 = Integer.toString(ivNumber);
+        for (int i = 0; i < lvNumberStrVar1.length(); i++) {
+//            Get Char
+            char lvCurCh = lvNumberStrVar1.charAt(i);
+//            Conv Char->String
+            String lvCurStr = String.valueOf(lvCurCh);
+//            Conv String->Int
+            int lvCurInt = Integer.parseInt(lvCurStr);
+//            SumCurInt
+            rvSum = rvSum + lvCurInt;
+        }
+//        Посчитать сумму чисел в строке
+        return rvSum;
+    }
+}
